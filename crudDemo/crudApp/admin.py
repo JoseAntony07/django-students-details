b6b37ac6bd6c387e90c5adec6f252d1de7f9fcbe
JoseAntony07 <87295492+JoseAntony07@users.noreply.github.com>
2021-07-12 11:22:13 +0530

    Delete crudDemo/crudApp directory

--- crudDemo/crudApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from crudApp.models import Student
-
-class StudentAdmin(admin.ModelAdmin):
-    list=['sRollNo','sname','sclass','sDateofbirth']
-
-admin.site.register(Student,StudentAdmin)
-
-
